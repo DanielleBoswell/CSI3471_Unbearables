e012513d666554592ac1431d8148f6fbcebf0544
WabbitGirl <danituongvy@gmail.com>
2023-10-14 14:40:18 -0500

    creating java maven project

---
+++ CruiseLink/com.csi3471.unbearables.maven.cruiselink/src/test/java/com/csi3471/unbearables/maven/cruiselink/AppTest.java
@@ -0,0 +1,20 @@
+package com.csi3471.unbearables.maven.cruiselink;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
