a9d675f104489d27c8863d9c906acff8efa27871
Spammck <108024146+Spammck@users.noreply.github.com>
2023-10-15 21:42:45 -0500

    CorporationClass
    Created corporation class and basic getters/setters

---
+++ Corporation.java
@@ -0,0 +1,47 @@
+/* Team: UnBEARables
+ * Course: CSI 3471
+ * Project: CruiseLink
+ * Filename: Corporation.java
+ * Creation Date: 10/15/2023
+ * Modified Date: 10/15/2023
+ * Description: Contains the Corporation class
+ *
+ */
+public class Corporation {
+    private String name;
+    private int numPeople;
+    private double discount;
+    private boolean discountAvailable;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumPeople() {
+        return numPeople;
+    }
+
+    public void setNumPeople(int numPeople) {
+        this.numPeople = numPeople;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public boolean isDiscountAvailable() {
+        return discountAvailable;
+    }
+
+    public void setDiscountAvailable(boolean discountAvailable) {
+        this.discountAvailable = discountAvailable;
+    }
+}
