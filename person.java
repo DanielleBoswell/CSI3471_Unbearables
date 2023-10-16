9d0f3d729b87fca59c101704bfbb07a0e6062d28
nickrevard9 <nickrevard@gmail.com>
2023-10-16 13:21:12 -0500

    person java created

---
+++ person.java
@@ -0,0 +1,51 @@
+package person;
+
+public class person {
+    String name;
+    int age;
+    String username;
+    String password;
+    String email;
+
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
