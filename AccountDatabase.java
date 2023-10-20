7bb3a9dfab5db61993047302056791ae393dca65
Spammck <108024146+Spammck@users.noreply.github.com>
2023-10-20 10:41:13 -0500

    added map and get person operations

--- AccountDatabase.java
+++ AccountDatabase.java
@@ -1,6 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class AccountDatabase {
-    
+    private Map<Integer,person> accounts = new HashMap<>();
 
+    public AccountDatabase(Map<Integer, person> accounts) {
+        this.accounts = accounts;
+    }
 
+    public boolean personExists(person p){
+        return (accounts.containsKey(p));
+    }
 
+    public person returnPerson(person p){
+        return accounts.get(p);
+    }
 }
+
