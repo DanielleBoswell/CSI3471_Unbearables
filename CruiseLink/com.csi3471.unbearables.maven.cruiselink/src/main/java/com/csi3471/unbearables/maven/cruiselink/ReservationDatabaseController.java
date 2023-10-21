67cebe862076c68e9e831d6cfdc40ba1890af555
Spammck <108024146+Spammck@users.noreply.github.com>
2023-10-21 15:32:00 -0500

    Added addReservation method to ReservationDatabaseController

--- CruiseLink/com.csi3471.unbearables.maven.cruiselink/src/main/java/com/csi3471/unbearables/maven/cruiselink/ReservationDatabaseController.java
+++ CruiseLink/com.csi3471.unbearables.maven.cruiselink/src/main/java/com/csi3471/unbearables/maven/cruiselink/ReservationDatabaseController.java
@@ -1,5 +1,14 @@
 package person.CruiseLink.com.csi3471.unbearables.maven.cruiselink.src.main.java.com.csi3471.unbearables.maven.cruiselink;
 
-public class ReservationDatabaseController {
+import java.util.ArrayList;
 
+public class ReservationDatabaseController {
+    void addReservation(ReservationDatabase database, Person p, Reservation r){
+        ArrayList<Reservation> tmp;
+        if(database.reservationMap.containsKey(p)){
+            tmp = database.reservationMap.get(p);
+            tmp.add(r);
+            database.reservationMap.put(p,tmp);
+        }
+    }
 }
