21316a9e5a66ca4db7012aaa156aed54ee2311a8
HoangKyle03 <hoang.kyle03@gmail.com>
2023-10-21 15:49:41 -0500

    implemented printRoom test for RoomTester

--- CruiseLink/com.csi3471.unbearables.maven.cruiselink/src/test/java/com/csi3471/unbearables/maven/cruiselink/RoomTester.java
+++ CruiseLink/com.csi3471.unbearables.maven.cruiselink/src/test/java/com/csi3471/unbearables/maven/cruiselink/RoomTester.java
@@ -1,12 +1,23 @@
 package person.CruiseLink.com.csi3471.unbearables.maven.cruiselink.src.test.java.com.csi3471.unbearables.maven.cruiselink;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import person.CruiseLink.com.csi3471.unbearables.maven.cruiselink.src.main.java.com.csi3471.unbearables.maven.cruiselink.Room;
 
 public class RoomTester {
     @Test
     void BeforeAll() {
 
+    }
+
+    @DisplayName("Reading from Room csv")
+    @ParameterizedTest(name = "{index => isSmoking={0}, bedType={1}, roomNumber={2}, numBeds={3}")
+    @CsvFileSource(resources = "/CruiseLink/com.csi3471.unbearables.maven.cruiselink/src/test/java/com/csi3471/unbearables/maven/cruiselink/Room.csv")
+    void printRoom(boolean isSmoking, Room.BedType bedType, int roomNumber, int numBeds) {
+        Room room = new Room(isSmoking, bedType, roomNumber, numBeds);
+        System.out.println(room.toString());
     }
 
     @Test
