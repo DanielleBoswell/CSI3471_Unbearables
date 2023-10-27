b97efd771e63501944ccb53132955cfcc27f7dd0
HoangKyle03 <hoang.kyle03@gmail.com>
2023-10-27 16:51:29 -0500

    added LoginLanding, MyMenuBar: LoginLanding requires addition of table of user's reservations

---
+++ Cruiselink/maven.cruiselink/src/Controller/LoginLandingControllerImpl.java
@@ -0,0 +1,13 @@
+package Cruiselink.maven.cruiselink.src.Controller;
+
+public class LoginLandingControllerImpl implements LoginLandingController {
+    @Override
+    public void onReserveCruisePressed() {
+        System.out.println("Reserve a Cruise pressed!");
+    }
+
+    @Override
+    public void onMyReservationsPressed() {
+        System.out.println("My Reservations pressed!");
+    }
+}
