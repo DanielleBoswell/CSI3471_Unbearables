5ad5d69bd06d7d91abf7e5bbdda8f298c4d7f738
Emma <e.aars@live.com>
2023-10-27 13:58:53 -0500

    added Login controller and interface

---
+++ Cruiselink/maven.cruiselink/src/Controller/LoginController.java
@@ -0,0 +1,7 @@
+package Cruiselink.maven.cruiselink.src.Controller;
+
+public interface LoginController {
+    void onLoginPressed(String username, String password);
+    void onSignUpPressed();
+    void onForgotPasswordPressed();
+}
---
+++ Cruiselink/maven.cruiselink/src/Controller/LoginControllerImpl.java
@@ -0,0 +1,29 @@
+package Cruiselink.maven.cruiselink.src.Controller;
+
+import javax.swing.*;
+
+public class LoginControllerImpl implements LoginController {
+
+    @Override
+    public void onLoginPressed(String username, String password) {
+
+        //Implement my logic here
+        System.out.println("Login button pressed");
+    }
+
+    @Override
+    public void onSignUpPressed() {
+
+        //Call signup GUI if sign up button is clicked
+        SwingUtilities.invokeLater(() -> {
+            new SignUpGUI().createGUI();
+        });
+    }
+
+    @Override
+    public void onForgotPasswordPressed() {
+
+        //Implement my forgot password logic here
+        System.out.println("Forgot Password link clicked");
+    }
+}
