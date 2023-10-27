1a65999a33958439b07b70613005bed09d5f0f09
Emma <e.aars@live.com>
2023-10-27 13:38:55 -0500

    added controllers found in old project

---
+++ Cruiselink/maven.cruiselink/src/Controller/ReservationDatabaseController.java
@@ -0,0 +1,21 @@
+package Cruiselink.maven.cruiselink.src.Controller;
+
+/*import CruiseLink.com.csi3471.unbearables.maven.cruiselink.src.Controller.cruiselink.Reservation;
+import CruiseLink.com.csi3471.unbearables.maven.cruiselink.src.Person.Guest;
+import CruiseLink.com.csi3471.unbearables.maven.cruiselink.src.Repository.ReservationDatabase;*/
+
+import java.util.ArrayList;
+
+public class ReservationDatabaseController {
+    void addReservation(ReservationDatabase database, Guest p, Reservation r){
+        ArrayList<Reservation> tmp = new ArrayList<>();
+        if(database.reservationMap.containsKey(p)){
+            tmp = database.reservationMap.get(p);
+            tmp.add(r);
+            database.reservationMap.put(p,tmp);
+        }else {
+            tmp.add(r);
+            database.reservationMap.put(p,tmp);
+        }
+    }
+}
---
+++ Cruiselink/maven.cruiselink/src/Controller/ReserveRoomController.java
@@ -0,0 +1,29 @@
+package Cruiselink.maven.cruiselink.src.Controller;
+
+/*import CruiseLink.com.csi3471.unbearables.maven.cruiselink.src.Controller.cruiselink.Reservation;
+import CruiseLink.com.csi3471.unbearables.maven.cruiselink.src.Controller.cruiselink.Room;
+import CruiseLink.com.csi3471.unbearables.maven.cruiselink.src.Controller.cruiselink.Ship;
+import CruiseLink.com.csi3471.unbearables.maven.cruiselink.src.Person.Guest;
+import CruiseLink.com.csi3471.unbearables.maven.cruiselink.src.Repository.ReservationDatabase;*/
+
+public class ReserveRoomController {
+    public void reserveRoom(ReservationDatabase d, Guest p, Ship s, Room r){
+        if(s.roomMap.containsKey(r.getRoomNumber()) && !s.roomMap.get(r.getRoomNumber()).getIsReserved()){
+            createReservation(d,p,s,r);
+            r.setReserved(Boolean.TRUE);
+        }else{
+            System.out.println("Already reserved");
+        }
+    }
+
+    public Reservation createReservation(ReservationDatabase d, Guest p, Ship s, Room r){
+
+        Reservation newRes = new Reservation(s.path.getStartDate(), s.path.getEndDate(), Boolean.FALSE, r);
+
+        ReservationDatabaseController resController = new ReservationDatabaseController();
+        resController.addReservation(d, p, newRes);
+
+
+        return newRes;
+    }
+}
