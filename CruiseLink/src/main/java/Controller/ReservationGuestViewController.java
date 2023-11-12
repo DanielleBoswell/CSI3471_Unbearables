76abf06901e3013b6ee169d8a9b729cfe7109508
WabbitGirl <danituongvy@gmail.com>
2023-11-12 16:27:19 -0600

    Mapping out implementation of viewing Guest Reservations

---
+++ CruiseLink/src/main/java/Controller/ReservationGuestViewController.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import Domain.Reservation;
+
+public class ReservationGuestViewController {
+
+    public void viewReservationsGuest(){
+
+    }
+
+    public void viewReservationGuest(Reservation r){
+
+    }
+
+    public boolean cancelReservationGuest(Reservation r){
+
+        return false;
+    }
+
+    public void confirmCancellationGuest(Reservation r){
+
+    }
+}
---
+++ CruiseLink/src/main/java/UI/ViewReservationsGUI.java
@@ -0,0 +1,15 @@
+package UI;
+
+import Domain.Reservation;
+
+import javax.swing.*;
+
+public class ViewReservationsGUI extends JPanel {
+    private UINavigator uiNavigator;
+
+    public ViewReservationsGUI(UINavigator uiNavigator){
+        this.uiNavigator = uiNavigator;
+    }
+
+
+}
