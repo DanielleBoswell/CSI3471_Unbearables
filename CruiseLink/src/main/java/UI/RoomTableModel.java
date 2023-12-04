dde517262033da6943efda92736bb3c3deb4203e
Spammck <108024146+Spammck@users.noreply.github.com>
2023-12-04 14:45:00 -0600

    edit

--- CruiseLink/src/main/java/UI/RoomTableModel.java
+++ CruiseLink/src/main/java/UI/RoomTableModel.java
@@ -1,15 +1,21 @@
 package UI;
 
+import Domain.Guest;
+
 import javax.swing.table.AbstractTableModel;
+import java.sql.*;
 
 
 class RoomTableModel extends AbstractTableModel {
+
     static Boolean DEBUG = false;
+
     public String[] columnNames = {"Quality Level",
             "Bed Type",
             "Number of Beds",
             "isSmoking"};
 
+    //create function
     public Object[][] data = {
             {"Executive", "Twin", "2", "True"},
             {"Comfort", "Full", "2", "False"},
@@ -17,6 +23,34 @@ class RoomTableModel extends AbstractTableModel {
             {"Comfort", "Queen", "3", "False"},
             {"Economy", "King", "1", "True"}
     };
+    Object[][] fillTable(Object[][] table) {
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            Connection con = DriverManager.getConnection("jdbc:derby:ex1connect;");
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Room");
+
+            if(rs.next()){
+                Guest tmp = new Guest();
+
+//                tmp.setId((long)id);
+//                tmp.setName(rs.getString("NAME"));
+//                tmp.setEmail(rs.getString("EMAIL"));
+//                tmp.setAge(rs.getInt("AGE"));
+//                tmp.setGender(rs.getString("GENDER"));
+//                tmp.setSalary(rs.getLong("SALARY"));
+
+
+            }
+
+            return table;
+
+        } catch (SQLException se) {
+            throw new RuntimeException(se);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public int getColumnCount() {
         return columnNames.length;
